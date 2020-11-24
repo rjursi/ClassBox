91cc51b4df28dc3b029569f96cb43a19854ae2ca
platina78 <ghdtjdwns87@gmail.com>
2020-11-24 11:23:00 +0900

    Add Custom MetroUI Form

--- ClassBox/ClassBox/Form1.Designer.cs
+++ ClassBox/ClassBox/Form1.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Controls.Add(this.metroLabel1);
+            this.metroTile1.Location = new System.Drawing.Point(34, 70);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(215, 130);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "21KB";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.metroTile1.UseSelectable = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Extreme;
+            this.metroLabel1.Location = new System.Drawing.Point(12, 9);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(48, 30);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "TXT";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(886, 501);
+            this.Controls.Add(this.metroTile1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Default;
+            this.Text = "ClassBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroTile1.ResumeLayout(false);
+            this.metroTile1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
 
--- ClassBox/ClassBox/Form1.cs
+++ ClassBox/ClassBox/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework;
 
 namespace ClassBox
 {
     public partial class Form1 : MetroFramework.Forms.MetroForm
     {
+       
         public Form1()
         {
             InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
